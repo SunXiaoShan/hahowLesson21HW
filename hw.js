36e5b342042cc28df39b5b570138aa6dbd339c20
Phineas <sunxiaoshan@Phineasde-MacBook-Pro.local>
2018-10-27 12:05:07 +0800

    rename

--- hw.js
+++ hw.js
@@ -17,13 +17,28 @@ function promptInput (prompt, handler)
     });
 }
 
-promptInput('app> ', input =>
-{
-    var count = input;
+function calculateProce(_count) {
+	var count = _count;
 	let oneRiceBox = count >= 10 ? 70 : 80;
 	let price = oneRiceBox * count;
-	console.log("便當總共 : " + price + "元")
+	return price
+}
+
+
+promptInput('app> ', input =>
+{
+	console.log("便當總共 : " + calculateProce(input) + "元")
 });
+
+
+describe('測試標題', function(){
+    it('測試內容', function(done){
+        // 進行測試
+    })
+    it('測試內容2', function(done){
+        // 進行測試
+    })
+})
 
 
 
